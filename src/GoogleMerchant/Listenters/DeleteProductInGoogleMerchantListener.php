fe8a972d5249129e1722fb7d523d9410263c70f6
Juan José Llorens <llorensjj@gmail.com>
2023-03-22 13:10:12 +0100

    Chore: wip

---
+++ src/GoogleMerchant/Listenters/DeleteProductInGoogleMerchantListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace DescomMarket\Feeds\GoogleMerchant\Listenters;
+
+use DescomMarket\Common\Events\Catalog\Products\ProductUnpublished;
+use DescomMarket\Common\Repositories\Catalog\Products\ProductRepository;
+use DescomMarket\Feeds\GoogleMerchant\Services\Products\ProductsDeleteService;
+
+class DeleteProductInGoogleMerchantListener
+{
+    public function handle(ProductUnpublished $event)
+    {
+        $productData = ProductRepository::get($event->productId);
+
+        if (!isset($productData['sku'])) {
+            logger()->error("Product without sku in DeleteProductInGoogleMerchantListener. ProductId: $event->productId");
+            return;
+        }
+
+        $productsDeleteService = new ProductsDeleteService();
+        $productsDeleteService->delete($productData['sku']);
+    }
+}
