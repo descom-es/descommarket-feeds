29d268cf2401b4c463c3eb6b308869fb35678576
Cesar <cesargb@gmail.com>
2023-05-25 15:14:57 +0200

    fix phpstan

--- src/Google/Models/Indexer.php
+++ src/Google/Models/Indexer.php
@@ -4,6 +4,11 @@ namespace DescomMarket\Feeds\Google\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $action
+ * @property string $url
+ * @property int $priority
+ */
 class Indexer extends Model
 {
     protected $table = 'google_indexing';
