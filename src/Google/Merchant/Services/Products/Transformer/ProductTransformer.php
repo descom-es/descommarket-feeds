5bdf6f53441fc6747f3b7d26a70bf726ef6f2647
Cesar <cesargb@gmail.com>
2023-06-12 12:17:33 +0200

    gmerchant, add category of google

--- src/Google/Merchant/Services/Products/Transformer/ProductTransformer.php
+++ src/Google/Merchant/Services/Products/Transformer/ProductTransformer.php
@@ -2,6 +2,8 @@
 
 namespace DescomMarket\Feeds\Google\Merchant\Services\Products\Transformer;
 
+use Closure;
+use DescomMarket\Feeds\Google\Merchant\Helper\CategoryTranslator;
 use Google\Service\ShoppingContent\Price;
 use Google\Service\ShoppingContent\Product;
 use Illuminate\Support\Str;
@@ -12,17 +14,26 @@ final class ProductTransformer
     {
         $product = new Product();
 
+        $categoryInGoogleMerchant = $productData['categoryInGoogleMerchant'] ?? null;
+
         $product->setId($productData['sku']);
         $product->setOfferId($productData['id']);
         $product->setChannel('online');
         $product->setTargetCountry('ES');
         $product->setContentLanguage('es');
         $product->setTitle($productData['name']);
+
+        if ( $productData['categoryInGoogleMerchant'] ?? null) {
+            $product->setGoogleProductCategory($productData['categoryInGoogleMerchant']);
+        }
+
         $product->setDescription((string)Str::of(html_entity_decode(strip_tags($productData['description'])))->limit(5000));
         $product->setLink($productData['url']);
         $product->setImageLink($productData['image']['url']);
         $product->setAvailability($productData['in_stock'] ? 'in stock' : 'out of stock');
-        $product->setProductTypes([self::productType($productData)]);
+
+        // $product->setProductTypes([self::productType($productData)]);
+
         $product->setCondition('new');
         $product->setBrand($productData['brand']['name'] ?? null);
         $product->setGtin($productData['gtin'] ?? null);
@@ -49,10 +60,14 @@ final class ProductTransformer
             $product->setSalePrice($price);
         }
 
-        $sectionSlug = $productData['categories'][0]['slug'] ?? null;
-        $product->setCustomLabel0($sectionSlug);
-
         return $product;
+    }
+
+    private function transformerDriver(): Closure
+    {
+        return self::$transformerDriver ?? function ($productData) {
+            return $productData;
+        };
     }
 
     private static function offer($productData): ?float
