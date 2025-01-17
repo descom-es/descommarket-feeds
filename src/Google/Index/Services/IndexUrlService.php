6f9addd48db8da31a30c8487d3bde4adcc6f3ee8
Cesar Garcia <cesargb@gmail.com>
2025-01-17 11:26:42 +0100

    google merchant: mejorar la gestión de URLs no encontradas en el servicio de indexación (#16)

--- src/Google/Index/Services/IndexUrlService.php
+++ src/Google/Index/Services/IndexUrlService.php
@@ -4,6 +4,7 @@ namespace DescomMarket\Feeds\Google\Index\Services;
 
 use DescomMarket\Feeds\Google\GoogleServiceBuilder;
 use Google\Service\Indexing\UrlNotification;
+use Illuminate\Support\Facades\Http;
 
 class IndexUrlService
 {
@@ -15,10 +16,19 @@ class IndexUrlService
             return;
         }
 
+        if ($this->isNotFound($url)) {
+            return;
+        }
+
         $urlNotification = new UrlNotification();
         $urlNotification->setType('URL_UPDATED');
         $urlNotification->setUrl($url);
 
         GoogleServiceBuilder::googleIndex()->urlNotifications->publish($urlNotification);
+    }
+
+    private function isNotFound(string $url): bool
+    {
+        return Http::get($url)->notFound();
     }
 }
